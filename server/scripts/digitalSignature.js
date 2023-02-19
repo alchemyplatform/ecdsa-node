62cd862f512a0f9e8e17979d76265298c0ed36b3
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-02-19 22:13:58 +0300

    Address in hexa

--- server/scripts/digitalSignature.js
+++ server/scripts/digitalSignature.js
@@ -4,5 +4,8 @@ const { hexToBytes, toHex } = require("ethereum-cryptography/utils");
 
 const hashMessage = (message) => keccak256(Uint8Array.from(message));
 
-
+const pubKeyToAddress = (pubKey) => {
+  const hash = keccak256(pubKey.slice(1));
+  return toHex(hash.slice(-20)).toUpperCase();
+};
 
