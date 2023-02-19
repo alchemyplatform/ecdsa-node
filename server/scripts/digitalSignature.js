253bd1b009f0f44f679c903de198ca1f32658d39
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-02-19 22:12:47 +0300

    Hash the message

--- server/scripts/digitalSignature.js
+++ server/scripts/digitalSignature.js
@@ -1,12 +1,8 @@
-const secp = require('ethereum-cryptography/secp256k1');
+const secp = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak");
-const { toHex, utf8ToBytes } = require('ethereum-cryptography/utils');
+const { hexToBytes, toHex } = require("ethereum-cryptography/utils");
 
-function hashMessage(message) {
-  let bytes = utf8ToBytes(message);
-  let hash = keccak256(bytes);
-  return hash;
-}
+const hashMessage = (message) => keccak256(Uint8Array.from(message));
 
 
 
