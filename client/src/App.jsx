fb5b809addae3fd5b0f769f3b69749c7c388221a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-02-20 22:12:42 +0300

    Display the signature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,21 @@ import { useState } from "react";
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
-
+  const [privateKey,setPrivateKey] = useState("");
+  const [signature, setSignature] = useState("");
   return (
     <div className="app">
       <Wallet
         balance={balance}
+        privateKey = {privateKey}
+        setPrivateKey = {setPrivateKey}
         setBalance={setBalance}
         address={address}
         setAddress={setAddress}
+        signature = {signature}
+        setSignature = {setSignature}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Transfer setBalance={setBalance} address={address} privateKey={privateKey} />
     </div>
   );
 }
