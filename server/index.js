29d7b5ced500901071175ced827554d52c28a005
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-02-20 22:09:49 +0300

    Create hashMessage, signAddress and recoverKey

--- server/index.js
+++ server/index.js
@@ -7,10 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "04b7e289c022835d415569488c8c072a622aa1cbf5d956efea6ebcf41d83fe19b05213c6ef3c408517625605c244460fb30b6d582b48c629a7fa32c91a6d30547a": 100,
-  "0415126b6b2df78427c07af6a8466aa839f19f7e5a4dfb6e745eea376ad095b58ccf52656cb505d6808ffb1d5300aba6f04569e1e1d5a9624d679346b10ad321af": 50,
-  "044fb79348548c606f082cc2482187f577e9a39d9a4dfb5c14fec11eb10f9488b3692c11c66998f104c29aed1bfae24463e30f0f1d954760bcc0e4af01cd34cc06": 75,
+  "a17dc0fa60592e4c7ef410dde0b38dc0e75814e3": 100,
+  "bc6a84b7dce3e64a0b4c29e74184735192157d29": 50,
+  "c6f9462fe05aa3057f171b8049d1ff846ea6620b": 75,
 };
+  
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
@@ -19,17 +20,26 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  // TODO: signature and message
+  const { sender, recipient, amount, sign, recoveryBit } = req.body;
+  const message = {sender, amount,recipient};
+  const messageHash = hashMessage(JSON.stringify(message));
+  const recovered = secp.recoverPublicKey(messageHash, hexToBytes(sign), recoveryBit);
+
+  const addressOfSign = toHex(addressFromPublicKey(recovered));
+  console.log('addressOfSign:',addressOfSign);
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
-
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+  if(sender!==addressOfSign){
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      console.log("Public Key Verified");
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
   }
 });
 
@@ -42,12 +52,12 @@ function setInitialBalance(address) {
     balances[address] = 0;
   }
 }
-
-// Private key: 355db4e0530bc99783775c2c1bfd198fc843e9e17a3313d69c71b45253ef5f77
-// Public key: 04b7e289c022835d415569488c8c072a622aa1cbf5d956efea6ebcf41d83fe19b05213c6ef3c408517625605c244460fb30b6d582b48c629a7fa32c91a6d30547a
-
-// Private key: be6788c3716419d0028a3f8003c55ba36c0230b1624fc5d6f4841e3b8d0b1b9b
-// Public key: 0415126b6b2df78427c07af6a8466aa839f19f7e5a4dfb6e745eea376ad095b58ccf52656cb505d6808ffb1d5300aba6f04569e1e1d5a9624d679346b10ad321af
-
-// Private key: 49beed66929c4e2231e3b4a2af6ed32f3c597002128dfa691bc3c0555ba4239d
-// Public key: 044fb79348548c606f082cc2482187f577e9a39d9a4dfb5c14fec11eb10f9488b3692c11c66998f104c29aed1bfae24463e30f0f1d954760bcc0e4af01cd34cc06
+function hashMessage(message) {
+  const bytes = utf8ToBytes(message);
+  return keccak256(bytes);
+}
+function addressFromPublicKey(publicKey){
+  const addrBytes = publicKey.slice(1);
+  const hash = keccak256(addrBytes);
+  return hash.slice(-20);
+}
--- server/scripts/digitalSignature.js
+++ server/scripts/digitalSignature.js
@@ -2,24 +2,21 @@ const secp = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak");
 const { hexToBytes, toHex } = require("ethereum-cryptography/utils");
 
-const hashMessage = (message) => keccak256(Uint8Array.from(message));
+function hashMessage(message) {
+  return keccak256(utf8ToBytes(message));
+}
 
-const pubKeyToAddress = (pubKey) => {
-  const hash = keccak256(pubKey.slice(1));
-  return toHex(hash.slice(-20)).toUpperCase();
-};
+const signMessage = (message, privateKey) => {
+  return secp.sign(hashMessage(message), privateKey, { recovered: true });
+}
 
-const signatureToPubKey = (message, signature) => {
-  const hash = hashMessage(message);
-  const fullSignatureBytes = hexToBytes(signature);
-  const recoveryBit = fullSignatureBytes[0];
-  const signatureBytes = fullSignatureBytes.slice(1);
+const recoverKey = (message, signature, recoveryBit) => {
+  return secp.recoverPublicKey(hashMessage(message), signature, recoveryBit);
+}
 
-  return secp.recoverPublicKey(hash, signatureBytes, recoveryBit);
-};
 
 module.exports = {
   hashMessage,
-  pubKeyToAddress,
-  signatureToPubKey,
+  signMessage,
+  recoverKey,
 };
