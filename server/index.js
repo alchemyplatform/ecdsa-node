0905cd252e5d2ed966329986c2a61213467f8f64
Shivansh Chauhan <greatshiva773@gmail.com>
2023-04-04 14:45:30 +0530

    Updated App and Server side

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const generate=require("./generate")
 const cors = require("cors");
 const port = 3042;
 
@@ -7,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0477d6d89efe313dcc63599dbe0aff9f7a84129fcd00c8c952b4eba35da41547e25d69132809fe620cbdccf5fcb3f2f053842a302fbf660635c4e45f693732fd8c": 100,
+  "041e7b0cec9217b3e2604f752f3805f52f10935aa51d8d4e0b1bce953f5cc95768ca298942265994e45695122c8421fb2b24f857cd4a9091f8c2fd75544cdf6dfa": 50,
+  "048e2bd0844c517a2cf8877c7aa05a737ebfda87581f198e6d84e3e7feb766ca6ccace598e16192081960b4e4cc5ce5a95bdc5bad61a5cd6efdc7550e619f66cab": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +20,10 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { message, signature } = req.body;
+  const { recipient, amount } = message;
+  const pk=generate.signatureToPk(message,signature);
+  const sender=generate.pktoAddress(pk);
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
